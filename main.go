15fc7242c49e0ce9537cca5a8d111aa8e831a13d
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2024-10-25 14:08:54 +0900

    add config file

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/Ecc-asplay/backend/util"
+)
 
 func main() {
-	fmt.Println("Testing")
+	config, err := util.LoadConfig("./")
+	if err != nil {
+		log.Println("app.env 見つけてない")
+	}
+
+	psql, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Println("DB 接続できない")
+	}
+
+	defer psql.Close()
+
 }
