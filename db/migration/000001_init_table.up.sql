d6420342618c1f63e0a2d44f2c60c317cd456a80
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2024-10-31 14:02:20 +0900

    update 10/31

--- db/migration/000001_init_table.up.sql
+++ db/migration/000001_init_table.up.sql
@@ -2,15 +2,16 @@ CREATE TABLE "User" (
     "UserID" UUID UNIQUE PRIMARY KEY NOT NULL,
     "Name" VARCHAR NOT NULL,
     "Email" VARCHAR UNIQUE NOT NULL,
-    "Age" INT NOT NULL,
-    "Gender" VARCHAR NOT NULL,
-    "Role" VARCHAR NOT NULL,
+    "Birth" DATE NOT NULL,
+    "Gender" ENUM NOT NULL,
+    "IsPrivacy" BOOL NOT NULL DEFAULT FALSE,
     "Disease" VARCHAR NOT NULL,
     "Condition" VARCHAR NOT NULL,
+    "Password" VARCHAR NOT NULL,
     "Certification" BOOLEAN DEFAULT FALSE,
-    "CreatedAt" TIMESTAMPTZ NOT NULL DEFAULT (NOW()),
-    "HashPassword" VARCHAR NOT NULL,
-    "ResetPasswordAt" TIMESTAMPTZ DEFAULT (NOW())
+    "ResetPasswordAt" TIMESTAMP DEFAULT (NOW()),
+    "CreatedAt" TIMESTAMP NOT NULL DEFAULT (NOW()),
+    "UpdatedAt" TIMESTAMP NOT NULL DEFAULT (NOW())
 );
 
 CREATE TABLE "Posts" (
@@ -18,39 +19,53 @@ CREATE TABLE "Posts" (
     "PostsID" UUID UNIQUE PRIMARY KEY NOT NULL,
     "ShowID" VARCHAR NOT NULL,
     "Title" VARCHAR NOT NULL,
+    "Feel" ENUM NOT NULL,
     "Content" VARCHAR NOT NULL,
-    "Media" BYTEA[],
+    "Reaction" INT NOT NULL,
+    "Image" VARBINARY,
     "IsSensitive" BOOLEAN DEFAULT FALSE,
     "Status" VARCHAR NOT NULL,
-    "CreatedAt" TIMESTAMPTZ NOT NULL DEFAULT (NOW()),
-    "EditedAt" TIMESTAMPTZ DEFAULT (NOW())
+    "CreatedAt" TIMESTAMP NOT NULL DEFAULT (NOW()),
+    "UpdatedAt" TIMESTAMP DEFAULT (NOW())
 );
 
-CREATE TABLE "UserComment" (
+CREATE TABLE "Comments" (
+    "CommentID" UUID UNIQUE PRIMARY KEY NOT NULL,
     "UserID" UUID NOT NULL,
     "PostsID" UUID NOT NULL,
+    "Status" ENUM NOT NULL,
+    "IsPublic" BOOL NOT NULL,
     "Comment" VARCHAR NOT NULL,
-    "CreatedAt" TIMESTAMPTZ NOT NULL DEFAULT (NOW())
+    "Reaction" INT NOT NULL,
+    "IsCensored" BOOL NOT NULL DEFAULT FALSE,
+    "CreatedAt" TIMESTAMP NOT NULL DEFAULT (NOW())
 );
 
-CREATE TABLE "Comment" (
-    "PostsID" UUID NOT NULL,
-    "Comment" VARCHAR NOT NULL,
-    "CreatedAt" TIMESTAMPTZ NOT NULL DEFAULT (NOW())
+CREATE TABLE "Bookmarks" (
+    "PostID" UUID NOT NULL,
+    "UserID" UUID NOT NULL,
+    "CreatedAt" TIMESTAMP NOT NULL DEFAULT (NOW())
 );
 
 CREATE TABLE "SearchRecord" (
     "SearchContent" VARCHAR NOT NULL,
-    "isUser" BOOLEAN DEFAULT FALSE,
-    "SearchedAt" TIMESTAMPTZ NOT NULL DEFAULT (NOW())
+    "isUser" BOOLEAN NOT NULL DEFAULT FALSE,
+    "SearchedAt" TIMESTAMP NOT NULL DEFAULT (NOW())
 );
 
 CREATE TABLE "Block" (
     "UserID" UUID NOT NULL,
     "BlockUserID" UUID NOT NULL,
     "Reason" VARCHAR NOT NULL,
-    "BlockAt" TIMESTAMPTZ NOT NULL DEFAULT (NOW()),
-    "UnBlockAt" TIMESTAMPTZ DEFAULT (NOW())
+    "BlockAt" TIMESTAMP NOT NULL DEFAULT (NOW()),
+    "UnBlockAt" TIMESTAMP DEFAULT (NOW())
+);
+
+CREATE TABLE "AdminUser" (
+    "Email" VARCHAR UNIQUE NOT NULL,
+    "Password" VARCHAR NOT NULL,
+    "Name" VARCHAR NOT NULL,
+    "Department" VARCHAR NOT NULL
 );
 
 CREATE TABLE "Tap" (
@@ -58,19 +73,13 @@ CREATE TABLE "Tap" (
     "Tap" VARCHAR[] NOT NULL
 );
 
-CREATE TABLE "SavePost" (
-    "UserID" UUID NOT NULL,
-    "PostID" UUID NOT NULL,
-    "SaveAt" TIMESTAMPTZ NOT NULL DEFAULT (NOW())
-);
-
 CREATE TABLE "Token" (
-    "TokenID" UUID UNIQUE NOT NULL,
+    "Token" VARCHAR UNIQUE NOT NULL,
     "Email" VARCHAR NOT NULL,
+    "Role" ENUM NOT NULL,
     "Status" VARCHAR NOT NULL,
-    "Role" VARCHAR NOT NULL,
-    "TakeAt" TIMESTAMPTZ NOT NULL DEFAULT (NOW()),
-    "ExpiresAt" TIMESTAMPTZ NOT NULL
+    "TakeAt" TIMESTAMP NOT NULL DEFAULT (NOW()),
+    "ExpiresAt" TIMESTAMP NOT NULL
 );
 
 ALTER TABLE "Posts"
@@ -81,7 +90,7 @@ ALTER TABLE "Posts"
             "UserID"
         );
 
-ALTER TABLE "UserComment"
+ALTER TABLE "Comments"
     ADD FOREIGN KEY (
         "UserID"
     )
@@ -89,7 +98,7 @@ ALTER TABLE "UserComment"
             "UserID"
         );
 
-ALTER TABLE "UserComment"
+ALTER TABLE "Comments"
     ADD FOREIGN KEY (
         "PostsID"
     )
@@ -97,12 +106,20 @@ ALTER TABLE "UserComment"
             "PostsID"
         );
 
-ALTER TABLE "Comment"
+ALTER TABLE "Bookmarks"
     ADD FOREIGN KEY (
-        "PostsID"
+        "PostID"
     )
         REFERENCES "Posts" (
             "PostsID"
+        );
+
+ALTER TABLE "Bookmarks"
+    ADD FOREIGN KEY (
+        "UserID"
+    )
+        REFERENCES "User" (
+            "UserID"
         );
 
 ALTER TABLE "Block"
@@ -114,22 +131,6 @@ ALTER TABLE "Block"
         );
 
 ALTER TABLE "Tap"
-    ADD FOREIGN KEY (
-        "PostID"
-    )
-        REFERENCES "Posts" (
-            "PostsID"
-        );
-
-ALTER TABLE "SavePost"
-    ADD FOREIGN KEY (
-        "UserID"
-    )
-        REFERENCES "User" (
-            "UserID"
-        );
-
-ALTER TABLE "SavePost"
     ADD FOREIGN KEY (
         "PostID"
     )
