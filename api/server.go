29a8e31556e4a4f2921a408aa1be5fbb1a907198
yuta <kurenuzon99@gmail.com>
2024-11-11 13:39:32 +0900

    commit

--- api/server.go
+++ api/server.go
@@ -19,7 +19,17 @@ func SetupRouter(config *util.Config, store db.Querier) (Server, error) {
 	}
 	r := gin.Default()
 
-	r.GET("/", server.Createuser)
+	// メモ：命名が気になるCreateuser→CreateUser
+	// ユーザー関連のルート定義
+	r.POST("/users", server.Createuser)
+	r.DELETE("/users/:id", server.Deleteuser)
+	r.GET("/users/:id", server.GetUserData)
+	r.PUT("/users/:id/password", server.ResetPassword)
+	r.PUT("/users/:id/disease-condition", server.UpdateDiseaseAndCondition)
+	r.PUT("/users/:id/email", server.UpdateEmail)
+	r.PUT("/users/:id/privacy", server.UpdateIsPrivacy)
+	r.PUT("/users/:id/name", server.UpdateName)
+	r.POST("/login", server.LoginUser)
 
 	server.router = r
 
--- api/user.go
+++ api/user.go
@@ -1,7 +1,27 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	db "github.com/Ecc-asplay/backend/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
 
 func (s *Server) Createuser(ctx *gin.Context) {
+	// 確認用記述(後で消す)
+	var req db.CreateUserParams
+	// ↑使わないほうが良い(ハッシュパスワードなどまだない情報があり、エラーがおこるかも？)
 
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	user, err := s.store.CreateUser(ctx, req)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, user)
 }
