c2c1165d47835a377c93ac88bfdbb15a4fefc367
yuta <kurenuzon99@gmail.com>
2024-12-02 14:14:20 +0900

    commit

--- api/searchrecord.go
+++ api/searchrecord.go
@@ -1,1 +1,34 @@
 package api
+
+import (
+	"net/http"
+
+	db "github.com/Ecc-asplay/backend/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type CreateSearchedRecordRequest struct {
+	SearchContent string `json:"search_content" binding:"required"`
+	IsUser        bool   `json:"is_user"`
+}
+
+func (s *Server) CreateSearchRecord(ctx *gin.Context) {
+	var req CreateSearchedRecordRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateSearchedRecordParams{
+		SearchContent: req.SearchContent,
+		IsUser:        req.IsUser,
+	}
+
+	record, err := s.store.CreateSearchedRecord(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create search record"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, record)
+}
