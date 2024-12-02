27af0b939d4efcbe7256fad38942e221c35dea7a
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2024-12-02 11:44:04 +0900

    bookmark Done

--- api/bookmarks.go
+++ api/bookmarks.go
@@ -9,28 +9,63 @@ import (
 	db "github.com/Ecc-asplay/backend/db/sqlc"
 )
 
-type createBookmarkRequest struct {
+type bookmarkRequest struct {
 	PostID uuid.UUID `json:"post_id"`
+	USERID uuid.UUID `json:"user_id"`
 }
 
 func (s *Server) CreateBookmark(ctx *gin.Context) {
-	var req createBookmarkRequest
+
+	var req bookmarkRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
-	// authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
-
 	data := db.CreateBookmarksParams{
 		PostID: req.PostID,
-		// UserID: authPayload.UserID,
+		UserID: req.USERID,
 	}
 
-	bookmark, err := s.store.CreateBookmarks(ctx, data)
+	createBookmark, err := s.store.CreateBookmarks(ctx, data)
 	if err != nil {
 		handleDBError(ctx, err)
 	}
 
-	ctx.JSON(http.StatusCreated, bookmark)
+	ctx.JSON(http.StatusCreated, createBookmark)
+}
+
+func (s *Server) DeleteBookmark(ctx *gin.Context) {
+	var req bookmarkRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	data := db.DeleteBookmarksParams{
+		PostID: req.PostID,
+		UserID: req.USERID,
+	}
+
+	err := s.store.DeleteBookmarks(ctx, data)
+	if err != nil {
+		handleDBError(ctx, err)
+	}
+
+	ctx.Status(http.StatusOK)
+}
+
+func (s *Server) GetBookmark(ctx *gin.Context) {
+	var req bookmarkRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	bookmark, err := s.store.GetAllBookmarks(ctx, req.USERID)
+	if err != nil {
+		handleDBError(ctx, err)
+	}
+
+	ctx.JSON(http.StatusOK, bookmark)
 }
